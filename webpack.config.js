17c748506deaab4ced1f45a36f025f8ebe23a22f
Yvonne Pon <pon-y@github.com>
2019-02-07 15:18:08 -0800

    Change file system to work on localhost:8080

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
 module.exports = {
-  entry: './client/index.js',
+  entry: `${__dirname}/client/src/index.jsx`,
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'] 
-      }
-    ]
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx'],
   },
   output: {
-    path: __dirname + '/dist',
+    path: `${__dirname}/dist`,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './client/dist',
+  },
 };
