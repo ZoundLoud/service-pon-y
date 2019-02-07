be93701d238b946a339112ba7f2e951870561b49
Yvonne Pon <pon-y@github.com>
2019-02-07 12:27:22 -0800

    Add table schema for song_description

--- schema.sql
+++ schema.sql
@@ -14,7 +14,17 @@ CREATE TABLE artist_info (
     no_of_tracks INT(20),
     pro_status INT(1) NOT NULL,
     is_followed INT(1) NOT NULL
+);
 
+CREATE TABLE song_description (
+    id INT(200) PRIMARY KEY AUTO_INCREMENT,
+    songname TEXT(300),
+    artistname TEXT(300),
+    licence TEXT(150),
+    descriptiontext TEXT(4000),
+    releasedby TEXT(150),
+    releasedate DATE,
+    pline TEXT(150)
 );
 -- //to run, open mysql 5.7 
 -- and use the following command from the root directory:
@@ -31,4 +41,16 @@ INSERT INTO artist_info(
     is_followed
     )
 VALUES ('KITTEH', 'https://placekitten.com/150/150', 20, 15, 0, 1),
-        ('BAR', 'https://placebear.com/200/300', 2000, 15, 0, 1)
+        ('BAR', 'https://placebear.com/200/300', 2000, 15, 0, 1);
+
+
+INSERT INTO song_description(
+    songname,
+    artistname,
+    licence,
+    descriptiontext,
+    releasedby,
+    releasedate,
+    pline
+    )
+VALUES ('Beep Beep Like a Sheep', 'BAR', NULL, 'one two three cake bird owl nope', null, null, null)
