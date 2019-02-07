0213946a0a8b6007dc47b789d757f61bbbb998cf
Yvonne Pon <pon-y@github.com>
2019-02-07 12:01:07 -0800

    Add table for artist_info and small sample data

--- schema.sql
+++ schema.sql
@@ -12,10 +12,23 @@ CREATE TABLE artist_info (
     avatar_picture VARCHAR(200),
     no_of_followers INT(20),
     no_of_tracks INT(20),
-    pro_status BINARY(1) NOT NULL,
-    is_followed BINARY(1) NOT NULL
+    pro_status INT(1) NOT NULL,
+    is_followed INT(1) NOT NULL
 
-)
+);
 -- //to run, open mysql 5.7 
 -- and use the following command from the root directory:
 -- source ./schema.sql; 
+
+-- dummy data for artist_info below: 
+
+INSERT INTO artist_info(
+    artistname,
+    avatar_picture,
+    no_of_followers,
+    no_of_tracks,
+    pro_status,
+    is_followed
+    )
+VALUES ('KITTEH', 'https://placekitten.com/150/150', 20, 15, 0, 1),
+        ('BAR', 'https://placebear.com/200/300', 2000, 15, 0, 1)
