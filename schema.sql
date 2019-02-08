e98b83d395d49f514ad1f7f6c0bcea7da667be3b
Yvonne <>
2019-02-08 15:32:00 -0800

    Add fixed schema.sql

--- schema.sql
+++ schema.sql
@@ -4,30 +4,29 @@ CREATE DATABASE zoundloud;
 
 USE zoundloud;
 
--- schema for artist_info table. 
 CREATE TABLE artist_info (
-    -- field name TYPE(#),
-    id INT(200) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    artistname VARCHAR(200) NOT NULL,
-    avatar_picture VARCHAR(200),
-    no_of_followers INT(20),
-    no_of_tracks INT(20),
-    pro_status INT(1) NOT NULL,
-    is_followed INT(1) NOT NULL
+  id INT(100) PRIMARY KEY AUTO_INCREMENT,
+  artistname TEXT(150),
+  avatar_picture TEXT(200),
+  no_of_followers INT(20),
+  no_of_tracks INT(20),
+  pro_status INT(1), 
+  is_followed INT(1)
 );
 
 CREATE TABLE song_description (
-    id INT(200) PRIMARY KEY AUTO_INCREMENT,
-    songname TEXT(300),
-    artistname TEXT(300),
-    licence TEXT(150),
-    descriptiontext TEXT(4000),
-    releasedby TEXT(150),
-    releasedate DATE,
-    pline TEXT(150),
-    tags TEXT(200),
-);
+  id INT(100) PRIMARY KEY AUTO_INCREMENT,
+  songname TEXT(200),
+  artistname TEXT(200), 
+  licence TEXT(200), 
+  descriptiontext TEXT(4000), 
+  releasedby TEXT(200), 
+  releasedate DATE, 
+  pline TEXT(200), 
+  tags TEXT(200) 
+)
 
+-- const sqlsong = 'INSERT INTO song_description(songname, artistname, licence, descriptiontext, releasedby, releasedate, pline, tags ) VALUES ?';
 -- //to run, open mysql 5.7 
 -- and use the following command from the root directory:
 -- source ./schema.sql; 
