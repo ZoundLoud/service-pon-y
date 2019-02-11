b26e5ad06e5e2fcabe955807549fdcfcbeaa82ec
Yvonne <>
2019-02-10 21:58:59 -0800

    Render additional fields to artist info widget

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,13 +9,39 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      value: 0,
+      name: artistData[0].artist_name,
+      avatar: artistData[0].avatar_picture,
+      followCount: artistData[0].no_of_followers,
+      trackCount: artistData[0].no_of_tracks,
+      isFollowing: artistData[0].is_followed,
     };
+
+    this.toggleFollow = this.toggleFollow.bind(this);
+  }
+
+  toggleFollow() {
+    const { isFollowing } = this.state;
+    this.setState({
+      isFollowing: !isFollowing,
+    });
   }
 
   render() {
+    const {
+      name, avatar, followCount, trackCount, isFollowing,
+    } = this.state;
     return (
-      <div>{songData[0].description_text}</div>
+      <div className="container">
+        <img src={avatar} alt="avatar" />
+        <div id="artistName">{name}</div>
+        <div id="followAndTrackCount">
+         (icon here):
+          {followCount}
+          (icon here):
+          {trackCount}
+        </div>
+        <button id="followingButton" onClick={this.toggleFollow}>{isFollowing ? 'Following' : 'Follow'}</button>
+      </div>
     );
   }
 }
