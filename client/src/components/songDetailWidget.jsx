e07d37788681f5eb3a30b082b668e200cfe48bc4
Yvonne <>
2019-02-11 17:17:47 -0800

    Add tags component

--- client/src/components/songDetailWidget.jsx
+++ client/src/components/songDetailWidget.jsx
@@ -7,19 +7,28 @@ class SongDetailWidget extends React.Component {
 
     this.state = {
       description: songData[0].description_text,
-      tags: songData[0].tags,
+      tags: songData[3].tags,
       license: songData[0].license,
       releasedBy: songData[0].released_by,
       releaseDate: songData[0].release_date,
-
+      pline: songData[0].p_line,
 
     };
   }
 
   render() {
     const {
-      description, license, releaseDate, releasedBy,
+      description, license, releaseDate, releasedBy, pline, tags,
     } = this.state;
+    const array = tags.split(' ');
+    const tag = array.map(el => (
+      <span className="tag">
+        {' '}
+        #
+        {el}
+        {' '}
+      </span>
+    ));
     return (
       <div className="songDetailWidget">
         <div>{description}</div>
@@ -30,10 +39,14 @@ class SongDetailWidget extends React.Component {
           <b>Release Date:</b>
           <div>{releaseDate}</div>
           {' '}
+          <b>P-line:</b>
+          <div>{pline}</div>
+          {' '}
           <b>Licensed By:</b>
           <div>{license}</div>
           {' '}
         </div>
+        {tag}
       </div>
     );
   }
