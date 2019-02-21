fe54dbf3666d3c77665ebff3173574ad509f711d
Yvonne <>
2019-02-21 10:25:05 -0800

    Clean away ebs files

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-//const db = require('./db');
+const db = require('./db');
 
 const app = express();
 
@@ -10,19 +10,19 @@ const port = 3210;
 
 app.use(bodyParser.json());
 app.use(cors());
-//app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/artistinfo', (req, res) => {
-  // db.artistWidget((data) => {
-  //   res.send(data);
-  // });
+  db.artistWidget((data) => {
+    res.send(data);
+  });
   res.send({});
 });
 
 app.get('/songinfo', (req, res) => {
-  // db.songDescription((data) => {
-  //   res.send(data);
-  // });
+  db.songDescription((data) => {
+    res.send(data);
+  });
   res.send({});
 });
 
