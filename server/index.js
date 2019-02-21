073a14cff12c4f5ebb87f741a93bbeea038d3cfb
Yvonne <>
2019-02-20 23:20:45 -0800

    stub out db for now

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-const db = require('./db');
+//const db = require('./db');
 
 const app = express();
 
@@ -13,15 +13,17 @@ app.use(cors());
 //app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/artistinfo', (req, res) => {
-  db.artistWidget((data) => {
-    res.send(data);
-  });
+  // db.artistWidget((data) => {
+  //   res.send(data);
+  // });
+  res.send({});
 });
 
 app.get('/songinfo', (req, res) => {
-  db.songDescription((data) => {
-    res.send(data);
-  });
+  // db.songDescription((data) => {
+  //   res.send(data);
+  // });
+  res.send({});
 });
 
 app.listen(port, () => console.log(`LISTENING ON PORT ${port}`));
